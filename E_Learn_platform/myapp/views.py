0abd299d0f0468b46e1331fe5d06d14c845b06c8
AlexVidenov1 <alexander_videnov@yahoo.com>
2024-08-02 10:17:34 +0300

    changes in views.py file register method

--- E_Learn_platform/myapp/views.py
+++ E_Learn_platform/myapp/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate
 from .forms import LoginForm, RegisterForm
 from django.shortcuts import render, redirect
 from .models import Student, Teacher
+from django.contrib.auth.models import User
 
 def home(request):
     return render(request, 'home.html')
@@ -27,16 +28,11 @@ def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
-            user.save()
-            user_type = form.cleaned_data.get('user_type')
-            if user_type == 'student':
-                Student.objects.create(user=user)
-            else:
-                Teacher.objects.create(user=user)
             username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
+            password = form.cleaned_data.get('password')
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+            Student.objects.create(user=user)
             login(request, user)
             return redirect('home')
     else:
@@ -56,7 +52,6 @@ def login_view(request):
                 login(request, user)
                 return redirect('home')
             else:
-                # Add an error message if authentication fails
                 form.add_error(None, 'Invalid username or password')
         else:
             print("Form is not valid")
